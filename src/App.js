8aa270617610d877e84abef0718c34d1e12aff4b
Lukasz Bolach <bravo1_lima1@yahoo.com>
2022-12-03 10:48:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Clock from './Clock';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    secondRatio: 0,
+    minuteRatio: 0,
+    hourRatio: 0,
+  }
+
+  componentDidMount() {               // setting clock. it runs setClock function with 1s interval
+    setInterval(() => {
+      this.setClock()
+    }, 1000)
+  }
+
+  setClock = () => {
+    const currentDate = new Date();
+    let secondRatio = currentDate.getSeconds() / 60;
+    let minuteRatio = (secondRatio + currentDate.getMinutes()) / 60;
+    let hourRatio = (minuteRatio + currentDate.getHours()) / 12;
+
+    this.setState({ secondRatio: secondRatio })
+    this.setState({ minuteRatio: minuteRatio })
+    this.setState({ hourRatio: hourRatio })
+
+  }
+
+  render() {
+    return (
+      <Clock />
+    );
+  }
 }
 
 export default App;
